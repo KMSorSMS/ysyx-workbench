72a4da8c0f5342ea96f7d5aac8e1740a3526c1ce
liam <yzwliam@126.com>
2024-09-17 14:19:48 +0800

    ✅ :fin double_control example

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,11 +1,20 @@
-#include "Vexample.h"
+#include "Vdouble_control.h"
 #include "verilated.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 int main(int argc, char **argv) {
   VerilatedContext *contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
-  Vexample *top = new Vexample{contextp};
+  Vdouble_control *top = new Vdouble_control{contextp};
   while (!contextp->gotFinish()) {
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
     top->eval();
+    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+    assert(top->f == (a ^ b));
   }
   delete top;
   delete contextp;
